d1eb6052223228120cff3408c1142ab59cf1ed16
Sai Gokul Teja <steja66@gmail.com>
2016-05-09 20:46:39 -0700

    Adding MenuController and MenuRepository classes

---
+++ TakeOutOrderingSystem/src/main/java/com/cmpe275/controllers/MenuController.java
@@ -0,0 +1,34 @@
+package com.cmpe275.controllers;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cmpe275.domain.Menu;
+import com.cmpe275.repository.MenuRepository;
+
+
+@RestController
+public class MenuController {
+	@Autowired
+	private MenuRepository menuRepository;
+	
+	@RequestMapping(value = "/getMenuItems", method = RequestMethod.POST)
+	@ResponseBody
+	public List<Menu> getItems(){
+		
+		Iterable<Menu> it =  menuRepository.findAll();
+		Iterator iterator = it.iterator();
+		List<Menu> menu = new ArrayList<>();
+		while(iterator.hasNext()){
+			menu.add((Menu)iterator.next());
+		}
+		return menu;
+	}
+}
---
+++ TakeOutOrderingSystem/src/main/java/com/cmpe275/repository/MenuRepository.java
@@ -0,0 +1,10 @@
+package com.cmpe275.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cmpe275.domain.Menu;
+
+public interface MenuRepository extends CrudRepository<Menu, Integer>{
+
+	
+}
