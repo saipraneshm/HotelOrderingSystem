181eed06215ef5ef75e9ea41ad4052083bb2c7df
srihaar atyam <krishnasrihaar@gmail.com>
2016-05-09 20:06:41 -0700

    Add OrderMenuRepo

---
+++ TakeOutOrderingSystem/src/main/java/com/cmpe275/repository/OrdersMenuRepository.java
@@ -0,0 +1,9 @@
+package com.cmpe275.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cmpe275.domain.Orders_Menu;
+
+public interface OrdersMenuRepository extends CrudRepository<Orders_Menu, Integer> {
+
+}
